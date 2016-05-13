3b6ab900d6b13e3f494c11f83a33e9559928c390
Chris Feher <fehercb@gmail.com>
2016-05-13 11:20:57 -0400

    Blah

--- SwiftBot/src/SBNetwork.swift
+++ SwiftBot/src/SBNetwork.swift
@@ -65,8 +65,8 @@ extension NSMutableURLRequest {
     func authenticate(username: String, password: String) -> NSMutableURLRequest {
         let authStr = "\(username):\(password)"
         let authData = authStr.dataUsingEncoding(NSUTF8StringEncoding)
-        print("DATA: \(authData?.base64EncodedDataWithOptions(.EncodingEndLineWithCarriageReturn))")
         let header = "Basic \(authData?.base64EncodedDataWithOptions(.EncodingEndLineWithCarriageReturn)))"
+        print(header)
         self.setValue(header, forHTTPHeaderField: "Authorization")
         return self
     }
